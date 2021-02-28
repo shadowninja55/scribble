5f4f21436af7ed0d2c65da9cca658fc539da3037
shadowninja55 <shadowninja1050@gmail.com>
2021-02-28 15:44:33 -0500

    client.v: 0.5 horizontal step fixed

--- client.v
+++ client.v
@@ -9,43 +9,52 @@ mut:
 	x int
 	y int
 	dragging bool
+	redraw bool = true
 }
 
 fn on_frame(mut app App) {
-	app.tui.clear()
-	app.tui.reset()
-
 	if app.dragging {
 		if app.y < app.height && app.x < app.width {
 			app.pixels[app.y][app.x] = true
 		}
 	}
+	
+	if !app.redraw { return }
+
+	app.tui.clear()
+	app.tui.set_bg_color(r: 255 g: 255 b: 255)
 
 	for y, row in app.pixels {
 		for x, pixel in row {
 			if pixel {
-				app.tui.set_cursor_position(x, y)
-				app.tui.set_bg_color(r: 255 g: 255 b: 255)
+				mut x_ := if x & 1 == 0 { x - 1 } else { x }
+				app.tui.set_cursor_position(x_, y)
 				app.tui.write("  ")
-				app.tui.reset()
 			}
 		}
 	}
 
+	app.tui.reset()
 	app.tui.flush()
 }
 
 fn on_event(event &ui.Event, mut app App) {
+	app.redraw = false
+
 	match event.typ {
 		.mouse_down {
 			app.dragging = true
 		}
 		.mouse_up {
-			app.dragging = false
+		app.dragging = false
 		}
 		.mouse_move, .mouse_drag {
 			app.x = event.x
 			app.y = event.y
+
+			if app.dragging {
+				app.redraw = true
+			}
 		}
 		else {}
 	}
@@ -58,7 +67,7 @@ fn main() {
     user_data: app
     frame_fn: on_frame
     event_fn: on_event
-    frame_rate: 30
+    frame_rate: 60
     hide_cursor: true
     window_title: "scribble"
   )
